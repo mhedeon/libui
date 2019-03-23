f3ed07e5780b2c9105ebe06283468d859851fc0a
mhedeon <maksged@gmail.com>
2019-03-23 18:09:27 +0200

    handled drawing outside the window

--- window/pixel.c
+++ window/pixel.c
@@ -2,6 +2,8 @@
 
 void	set_pixel(t_window *win, SDL_Color *color, int x, int y)
 {
+	if (x < 0 || x >= win->w || y < 0 || x >= win->h)
+		return;
 	win->buff[y * win->w + x] =
 		color->r << 24 |
 		color->g << 16 |
